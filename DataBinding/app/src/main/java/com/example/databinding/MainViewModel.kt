1d5fd62f3fb0fe4be84690f68dbf7f68fb9d36ef
Rohit Kumar <rohit7091407599@gmail.com>
2024-10-28 08:15:50 +0530

    Learn Binding Adapter and Glide

--- DataBinding/app/src/main/java/com/example/databinding/MainViewModel.kt
+++ DataBinding/app/src/main/java/com/example/databinding/MainViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class MainViewModel: ViewModel() {
 
     var quote = MutableLiveData<String>("The greatest glory in living lies not in never falling, but in rising every time we fall.")
-
+    var image = "https://th.bing.com/th/id/OIP.ZbLa3u6k8KGhDhaSpYOMQQHaIM?rs=1&pid=ImgDetMain"
     fun updateQuote() {
         quote.value = "Updated: The greatest glory in living lies not in never falling, but in rising every time we fall."
     }
