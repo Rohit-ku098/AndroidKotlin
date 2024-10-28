a0362ee63f681d510c69ae87a0282a70c1be8acd
Rohit Kumar <rohit7091407599@gmail.com>
2024-10-28 12:21:41 +0530

    Database Migration

--- SharedPrefAndDatabase/app/src/main/java/com/example/sharedprefanddatabase/database/Database.kt
+++ SharedPrefAndDatabase/app/src/main/java/com/example/sharedprefanddatabase/database/Database.kt
@@ -6,13 +6,25 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [UserEntity::class], version = 1)
+@Database(entities = [UserEntity::class], version = 2)
 @TypeConverters(Convertor::class)
 abstract class MyDatabase: RoomDatabase() {
     abstract fun userDao(): UserDAO
 
     companion object {
+        // Database Migration from version 1 to 2
+        /**
+         * When making any chenge in the database schema, we need to add migration
+         */
+        val migration_1_2 = object : Migration(1,2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE users ADD COLUMN isActive INTEGER NOT NULL DEFAULT 1")
+            }
+        }
+
         @Volatile
         private var instance: MyDatabase? = null
         fun getInstance(context: Context): MyDatabase {
@@ -22,7 +34,9 @@ abstract class MyDatabase: RoomDatabase() {
                         context.applicationContext,
                         MyDatabase::class.java,
                         "user_db"
-                    ).build()
+                        )
+                        .addMigrations(migration_1_2)
+                        .build()
                 }
             }
             return instance!!
